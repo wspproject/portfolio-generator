894290c456cf23128e8afdf752177d4d7e098660
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-03-25 22:10:33 +0100

    POR-15 - added calling DefaultMetatags in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
+import { Html, Head, Main, NextScript } from "next/document";
+import { DefaultMetatags } from "@/components/metatags/DefaultMetatags";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <DefaultMetatags />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
